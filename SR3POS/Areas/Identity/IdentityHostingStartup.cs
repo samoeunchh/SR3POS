5aa08aa1f33eebd4752240175b6697807a862aea
samoeun chhay <samoeunchh@gmail.com>
2021-10-22 15:28:09 +0700

    Add project files.

---
+++ SR3POS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SR3POS.Data;
+
+[assembly: HostingStartup(typeof(SR3POS.Areas.Identity.IdentityHostingStartup))]
+namespace SR3POS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
