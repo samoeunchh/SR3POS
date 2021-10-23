a3326a9b3cf7787ecb05a8cefcfcc49bf51b0d86
samoeun chhay <samoeunchh@gmail.com>
2021-10-23 14:02:48 +0700

    Add Product

--- SR3POS/Data/Migrations/20211023065553_AddProduct.Designer.cs
+++ SR3POS/Data/Migrations/20211023065553_AddProduct.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SR3POS.Data;
 
 namespace SR3POS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211023065553_AddProduct")]
+    partial class AddProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
